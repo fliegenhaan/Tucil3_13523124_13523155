746e92b9d4dcdd5f75b3d2ace0a5532fff311038
Abizzar <13523155@std.stei.itb.ac.id>
2025-05-19 08:31:05 +0700

    feat: visualize wall

--- src/visualizer/GUIVisualizer.java
+++ src/visualizer/GUIVisualizer.java
@@ -1,35 +1,33 @@
 package visualizer;
 
-import javax.swing.*;
-
 import entity.Board;
+import entity.Direction;
 import entity.Move;
 import entity.Node;
 import entity.Piece;
 import entity.Position;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
+import javax.swing.*;
 
 public class GUIVisualizer extends JFrame {
-    private List<Node> solution;
+    private final List<Node> solution;
     private int currentStep;
-    private BoardPanel boardPanel;
-    private JButton prevButton, nextButton, playButton, stopButton;
-    private JLabel stepLabel;
-    private JLabel moveLabel;
-    private Timer animationTimer;
+    private final BoardPanel boardPanel;
+    private final JButton prevButton, nextButton, playButton, stopButton;
+    private final JLabel stepLabel;
+    private final JLabel moveLabel;
+    private final Timer animationTimer;
     private boolean isPlaying;
     
     private static final int CELL_SIZE = 60;
     private static final int MARGIN = 20;
     private static final Color PRIMARY_COLOR = Color.RED;
-    private static final Color EXIT_COLOR = Color.GREEN;
     private static final Color LAST_MOVED_COLOR = Color.YELLOW;
     private static final Color BORDER_COLOR = Color.BLACK;
     private static final Color BACKGROUND_COLOR = Color.WHITE;
+    private static final Color WALL_COLOR = Color.BLACK;
     
     public GUIVisualizer(List<Node> solution) {
         this.solution = solution;
@@ -55,41 +53,29 @@ public class GUIVisualizer extends JFrame {
         stepLabel = new JLabel("Step: 0/" + (solution.size() - 1));
         moveLabel = new JLabel("Move: None");
         
-        prevButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (currentStep > 0) {
-                    currentStep--;
-                    updateView();
-                }
+        prevButton.addActionListener((ActionEvent e) -> {
+            if (currentStep > 0) {
+                currentStep--;
+                updateView();
             }
         });
         
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (currentStep < solution.size() - 1) {
-                    currentStep++;
-                    updateView();
-                }
+        nextButton.addActionListener((ActionEvent e) -> {
+            if (currentStep < solution.size() - 1) {
+                currentStep++;
+                updateView();
             }
         });
         
-        playButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!isPlaying) {
-                    startAnimation();
-                }
+        playButton.addActionListener((ActionEvent e) -> {
+            if (!isPlaying) {
+                startAnimation();
             }
         });
         
-        stopButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (isPlaying) {
-                    stopAnimation();
-                }
+        stopButton.addActionListener((ActionEvent e) -> {
+            if (isPlaying) {
+                stopAnimation();
             }
         });
         
@@ -102,20 +88,18 @@ public class GUIVisualizer extends JFrame {
         
         add(controlPanel, BorderLayout.SOUTH);
         
-        animationTimer = new Timer(500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (currentStep < solution.size() - 1) {
-                    currentStep++;
-                    updateView();
-                } else {
-                    stopAnimation();
-                }
+        animationTimer = new Timer(500, (ActionEvent e) -> {
+            if (currentStep < solution.size() - 1) {
+                currentStep++;
+                updateView();
+            } else {
+                stopAnimation();
             }
         });
         
-        int windowWidth = width * CELL_SIZE + 2 * MARGIN;
-        int windowHeight = height * CELL_SIZE + 2 * MARGIN + 100;
+        // tambahkan space untuk dinding
+        int windowWidth = (width + 2) * CELL_SIZE + 2 * MARGIN;
+        int windowHeight = (height + 2) * CELL_SIZE + 2 * MARGIN + 100;
         setSize(windowWidth, windowHeight);
         setLocationRelativeTo(null);
         
@@ -155,15 +139,16 @@ public class GUIVisualizer extends JFrame {
     }
     
     private class BoardPanel extends JPanel {
-        private int width;
-        private int height;
+        private final int width;
+        private final int height;
         private Board board;
         private Move lastMove;
         
         public BoardPanel(int width, int height) {
             this.width = width;
             this.height = height;
-            setPreferredSize(new Dimension(width * CELL_SIZE + 2 * MARGIN, height * CELL_SIZE + 2 * MARGIN));
+            // tambahkan ruang untuk dinding
+            setPreferredSize(new Dimension((width + 2) * CELL_SIZE + 2 * MARGIN, (height + 2) * CELL_SIZE + 2 * MARGIN));
             setBackground(BACKGROUND_COLOR);
         }
         
@@ -184,24 +169,26 @@ public class GUIVisualizer extends JFrame {
             Graphics2D g2d = (Graphics2D) g;
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             
-            char[][] grid = board.getGrid();
             char primaryPieceId = board.getPrimaryPieceId();
             Position exitPos = board.getExitPosition();
+            Direction exitDir = board.getExitDirection();
             char lastMovePieceId = lastMove != null ? lastMove.getPieceId() : ' ';
             
+            // gambar dinding di sekeliling board
+            drawWalls(g2d, exitPos, exitDir);
+            
+            // gambar grid lines untuk playing area
             g2d.setColor(BORDER_COLOR);
             for (int i = 0; i <= height; i++) {
-                g2d.drawLine(MARGIN, MARGIN + i * CELL_SIZE, MARGIN + width * CELL_SIZE, MARGIN + i * CELL_SIZE);
+                g2d.drawLine(MARGIN + CELL_SIZE, MARGIN + CELL_SIZE + i * CELL_SIZE, 
+                           MARGIN + CELL_SIZE + width * CELL_SIZE, MARGIN + CELL_SIZE + i * CELL_SIZE);
             }
             for (int j = 0; j <= width; j++) {
-                g2d.drawLine(MARGIN + j * CELL_SIZE, MARGIN, MARGIN + j * CELL_SIZE, MARGIN + height * CELL_SIZE);
+                g2d.drawLine(MARGIN + CELL_SIZE + j * CELL_SIZE, MARGIN + CELL_SIZE, 
+                           MARGIN + CELL_SIZE + j * CELL_SIZE, MARGIN + CELL_SIZE + height * CELL_SIZE);
             }
             
-            g2d.setColor(EXIT_COLOR);
-            g2d.fillRect(MARGIN + exitPos.getCol() * CELL_SIZE, MARGIN + exitPos.getRow() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-            g2d.setColor(BORDER_COLOR);
-            g2d.drawString("K", MARGIN + exitPos.getCol() * CELL_SIZE + CELL_SIZE / 2 - 5, MARGIN + exitPos.getRow() * CELL_SIZE + CELL_SIZE / 2 + 5);
-            
+            // gambar pieces di playing area
             for (Piece piece : board.getPieces().values()) {
                 Color pieceColor;
                 if (piece.getId() == primaryPieceId) {
@@ -214,11 +201,78 @@ public class GUIVisualizer extends JFrame {
                 
                 for (Position pos : piece.getPositions()) {
                     g2d.setColor(pieceColor);
-                    g2d.fillRect(MARGIN + pos.getCol() * CELL_SIZE + 1, MARGIN + pos.getRow() * CELL_SIZE + 1, CELL_SIZE - 1, CELL_SIZE - 1);
+                    g2d.fillRect(MARGIN + CELL_SIZE + pos.getCol() * CELL_SIZE + 1, 
+                               MARGIN + CELL_SIZE + pos.getRow() * CELL_SIZE + 1, 
+                               CELL_SIZE - 1, CELL_SIZE - 1);
                     g2d.setColor(BORDER_COLOR);
-                    g2d.drawString(String.valueOf(piece.getId()), MARGIN + pos.getCol() * CELL_SIZE + CELL_SIZE / 2 - 5, MARGIN + pos.getRow() * CELL_SIZE + CELL_SIZE / 2 + 5);
+                    g2d.drawString(String.valueOf(piece.getId()), 
+                                 MARGIN + CELL_SIZE + pos.getCol() * CELL_SIZE + CELL_SIZE / 2 - 5, 
+                                 MARGIN + CELL_SIZE + pos.getRow() * CELL_SIZE + CELL_SIZE / 2 + 5);
                 }
             }
+        }
+        
+        private void drawWalls(Graphics2D g2d, Position exitPos, Direction exitDir) {
+            g2d.setColor(WALL_COLOR);
+            
+            // gambar dinding atas
+            for (int j = 0; j < width + 2; j++) {
+                if (!(exitDir == Direction.UP && j == exitPos.getCol() + 1)) {
+                    g2d.fillRect(MARGIN + j * CELL_SIZE, MARGIN, CELL_SIZE, CELL_SIZE);
+                }
+            }
+            
+            // gambar dinding bawah
+            for (int j = 0; j < width + 2; j++) {
+                if (!(exitDir == Direction.DOWN && j == exitPos.getCol() + 1)) {
+                    g2d.fillRect(MARGIN + j * CELL_SIZE, MARGIN + (height + 1) * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+            
+            // gambar dinding kiri
+            for (int i = 1; i <= height; i++) {
+                if (!(exitDir == Direction.LEFT && i - 1 == exitPos.getRow())) {
+                    g2d.fillRect(MARGIN, MARGIN + i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+            
+            // gambar dinding kanan
+            for (int i = 1; i <= height; i++) {
+                if (!(exitDir == Direction.RIGHT && i - 1 == exitPos.getRow())) {
+                    g2d.fillRect(MARGIN + (width + 1) * CELL_SIZE, MARGIN + i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+            
+            // gambar border untuk exit (kotak kosong)
+            g2d.setColor(BORDER_COLOR);
+            int exitDisplayRow, exitDisplayCol;
+            
+            switch (exitDir) {
+                case UP -> {
+                    exitDisplayRow = 0;
+                    exitDisplayCol = exitPos.getCol() + 1;
+                }
+                case DOWN -> {
+                    exitDisplayRow = height + 1;
+                    exitDisplayCol = exitPos.getCol() + 1;
+                }
+                case LEFT -> {
+                    exitDisplayRow = exitPos.getRow() + 1;
+                    exitDisplayCol = 0;
+                }
+                case RIGHT -> {
+                    exitDisplayRow = exitPos.getRow() + 1;
+                    exitDisplayCol = width + 1;
+                }
+                default -> {
+                    return;
+                }
+            }
+            
+            // gambar border exit tanpa fill
+            g2d.drawRect(MARGIN + exitDisplayCol * CELL_SIZE, 
+                        MARGIN + exitDisplayRow * CELL_SIZE, 
+                        CELL_SIZE, CELL_SIZE);
         }
     }
 }
